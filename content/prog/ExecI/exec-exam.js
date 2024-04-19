81a937b5b5ad7a5017ee295e9494659e5a7e8606
Kodie <kodii.tulip@gmail.com>
2024-04-19 20:50:37 -0300

    Quartz sync: Apr 19, 2024, 8:50 PM

--- content/prog/ExecI/exec-exam.js
+++ content/prog/ExecI/exec-exam.js
@@ -1,5 +1,10 @@
 const execexam = ( p ) => {
 	let alun = [];
+	let menorNotas = {
+		1: 0,
+		2: 0,
+		3: 0,
+	};
 	
 	p.setup = () => {
 		p.createCanvas(400, 400);
@@ -18,8 +23,22 @@ const execexam = ( p ) => {
 	p.draw = () => {
 		p.background(220);
 		p.drawMatrixes();
-		p.text('Click On Screen to Regen', p.width/2, p.height * 5/6);
+		p.text(
+			'Click On Screen to Regen', 
+			p.width/2, 
+			p.height * 11/12
+		);
 	};
+
+	p.touchStarted = () => {
+        let pressedInCanvas = p.mouseX > 0 &&
+                            p.mouseX < p.width &&
+                            p.mouseY > 0 &&
+                            p.mouseY < p.height;
+        if (!pressedInCanvas) {
+            return false;
+        }
+    }
 
 	p.mousePressed = () => {
         let pressedInCanvas = p.mouseX > 0 &&
@@ -30,12 +49,18 @@ const execexam = ( p ) => {
             return;
         }
 
+        menorNotas = {
+            1: 0,
+            2: 0,
+            3: 0,
+        };
+
 		for (let i = 0; i < 10; i++) {
 			alun[i] = new StudentInfo(
 				i + 1,
-				Math.random(),
-				Math.random(),
-				Math.random()
+				Math.random() * 10,
+				Math.random() * 10,
+				Math.random() * 10
 			)
 		}
 	}
@@ -49,16 +74,29 @@ const execexam = ( p ) => {
 				p.rect(i * ts * 4, 0, ts * 4, ts * 2);
 			}
 		}
-		p.text('Aluno'     , ts + ts * 4 * 0, 0, ts * 2, ts * 2);
-		p.text('Prova1'    , ts + ts * 4 * 1, 0, ts * 2, ts * 2);
-		p.text('Prova2'    , ts + ts * 4 * 2, 0, ts * 2, ts * 2);
-		p.text('Prova3'    , ts + ts * 4 * 3, 0, ts * 2, ts * 2);
-		p.text('Média'     , ts + ts * 4 * 4, 0, ts * 2, ts * 2);
-		p.text('Menor Nota', ts + ts * 4 * 5, 0, ts * 6, ts * 2);
+		p.text('Aluno'     , ts * 4 * 0, 0, ts * 4, ts * 2);
+		p.text('Prova1'    , ts * 4 * 1, 0, ts * 4, ts * 2);
+		p.text('Prova2'    , ts * 4 * 2, 0, ts * 4, ts * 2);
+		p.text('Prova3'    , ts * 4 * 3, 0, ts * 4, ts * 2);
+		p.text('Média'     , ts * 4 * 4, 0, ts * 4, ts * 2);
+		p.text('Menor Nota', ts * 4 * 5, 0, ts * 8, ts * 2);
 		
 		for (let a = 0; a < alun.length; a++) {
 			let alu = alun[a];
 			alu.renderMarix(0, ts * 2 * (a + 1));
+		}
+		for (let f = 0; f < 3; f++) {
+			p.rect(
+				ts * 4 * (f + 1), 
+				ts * 2 * (alun.length + 1), 
+				ts * 4, ts * 2
+			);
+			p.text(
+				menorNotas[f + 1], 
+				ts * 4 * (f + 1), 
+				ts * 2 * (alun.length + 1), 
+				ts * 4, ts * 2
+			);
 		}
 	}
 	
@@ -75,6 +113,17 @@ const execexam = ( p ) => {
 			this.n1 = n1;
 			this.n2 = n2;
 			this.n3 = n3;
+			this.menornota = 0;
+			this.media = (n1 + n2 + n3) / 3;
+            let mi = p.min(n1, n2, n3);
+			if (this.n1 == mi) {
+				this.menornota = 1;
+			} else if (this.n2 == mi) {
+				this.menornota = 2;
+			} else if (this.n3 == mi) {
+				this.menornota = 3;
+			}
+			menorNotas[this.menornota] += 1;
 		}
 		
 		/**
@@ -95,19 +144,39 @@ const execexam = ( p ) => {
 			}
 			p.textAlign(p.CENTER,p.CENTER);
 			p.text(this.name, 0, 0, ts * 4, ts * 2);
-			p.text(this.n1.toFixed(2), ts *  4, 0, ts * 4, ts * 2);
-			p.text(this.n2.toFixed(2), ts *  8, 0, ts * 4, ts * 2);
-			p.text(this.n3.toFixed(2), ts * 12, 0, ts * 4, ts * 2);
-			let m = (this.n1 + this.n2 + this.n3) / 3;
-			p.text(m.toFixed(2), ts * 16, 0, ts * 4, ts * 2);
-			let mn = p.min(this.n1, this.n2, this.n3);
-			if (this.n1 == mn) {
-				p.text('Prova1', ts * 20, 0, ts * 8, ts * 2);
-			} else if (this.n2 == mn) {
-				p.text('Prova2', ts * 20, 0, ts * 8, ts * 2);
-			} else if (this.n3 == mn) {
-				p.text('Prova3', ts * 20, 0, ts * 8, ts * 2);
-			}
+			p.text(
+				this.n1.toFixed(2), 
+				ts *  4, 
+				0, ts * 4, 
+				ts * 2
+			);
+			p.text(
+				this.n2.toFixed(2), 
+				ts *  8, 
+				0, ts * 4, 
+				ts * 2
+			);
+			p.text(
+				this.n3.toFixed(2), 
+				ts * 12, 
+				0, ts * 4, 
+				ts * 2
+			);
+			
+			p.text(
+				this.media.toFixed(2), 
+				ts * 16, 
+				0, 
+				ts * 4, 
+				ts * 2
+			);
+			p.text(
+				`Prova ${this.menornota}`, 
+				ts * 20, 
+				0, 
+				ts * 8, 
+				ts * 2
+			);
 			p.translate(-x, -y);
 			p.pop();
 		}
